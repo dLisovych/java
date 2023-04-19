3df113fc9d00548b3bc2a08b2b3305783e7c7aa1
Serhii-Vivdenko <Sergejarty@gmail.com>
2023-04-19 15:02:36 +0200

    Test

---
+++ Homework1/src/Main.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner cs = new Scanner(System.in);
+        System.out.println("Input the radius");
+        //String input = cs.nextLine();
+        int a = Integer.parseInt(cs.nextLine());
+        double area = a * a * Math.PI;
+        double perimeter = 2 * Math.PI * a;
+
+        System.out.println("perimeter " + perimeter);
+        System.out.println("area      " + area);
+
+    }
+}
